7fa674db20718c7dcd539c2655a7537c800bbc91
João <joaopedro.devbr@gmail.com>
2024-08-16 18:40:24 -0300

    feat: applying open/closed with composition
    see that we can add (extend) components, based on smaller components.
    
    our input component doesn't get huge, it stays simple, with the possibility of adding children and extending it (labels, icons, etc.) working with composition

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,24 +1,22 @@
-import { Lock } from "lucide-react";
 import { InputHTMLAttributes } from "react";
 
-interface IInput extends InputHTMLAttributes<HTMLInputElement> {
-  label?: string;
-  showLeftIcon?: boolean;
+type TInput = InputHTMLAttributes<HTMLInputElement>;
+
+interface IInputLabel {
+  children: React.ReactNode;
+  id?: string;
 }
 
-function Input({ label, showLeftIcon, ...rest }: Readonly<IInput>) {
+function InputLabel({ children, id }: Readonly<IInputLabel>) {
+  return <label htmlFor={id}>{children}</label>;
+}
+
+function Input({ children, id, ...rest }: Readonly<TInput>) {
   return (
-    <>
-      <div style={{ display: "flex", flexDirection: "column", width: "100%" }}>
-        {label && <label htmlFor="input-with-label">{label}</label>}
-        <input {...rest} type="text" id="input-with-label" />
-      </div>
-      {showLeftIcon && (
-        <div>
-          <Lock />
-        </div>
-      )}
-    </>
+    <div style={{ display: "flex", flexDirection: "column", width: "100%" }}>
+      {children}
+      <input {...rest} type="text" id={id} />
+    </div>
   );
 }
-export { Input };
+export { Input, InputLabel };
--- src/pages/Solid.tsx
+++ src/pages/Solid.tsx
@@ -1,10 +1,21 @@
-import { Input } from "../components/Input";
+import { Atom } from "lucide-react";
+import { Input, InputLabel } from "../components/Input";
 
 function Solid() {
   return (
     <div>
       <h1>⚛️ Open-Closed Principle</h1>
-      <Input label="Username:" placeholder="You username" showLeftIcon={true} />
+      {/* Example 1: */}
+      {/* <Input label="Username" id="example" placeholder="You username" showLeftIcon={true} /> */}
+
+        {/* Example 2: */}
+      <Input id="example-two" placeholder="You username" >
+        <InputLabel>Example 2 - With children</InputLabel>
+      </Input>
+
+      <Input id="example-two-" placeholder="You username two" >
+        <InputLabel>Example 2.1 - With children <Atom /></InputLabel>
+      </Input>
     </div>
   );
 }
